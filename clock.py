4c1fb9856884267cc5d18ac82d5ec9097e4a465d
iwanaghi <lennon_basyou@yahoo.co.jp>
2019-06-22 07:15:01 +0900

    試しにherokuが動いているか確認

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import wfutv4re
 
 twische = BlockingScheduler()
 
-@twische.scheduled_job('interval',hours=24)
+@twische.scheduled_job('interval',minutes=1)
 def timed_job():
     wfutv4re.tweet()
 
