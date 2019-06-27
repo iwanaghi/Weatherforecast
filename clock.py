1a99f90503ddc5849547773de7d2155fbb26433a
iwanaghi <lennon_basyou@yahoo.co.jp>
2019-06-28 07:22:11 +0900

    さらにclock.py修正、,と.間違えた

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ twische = BlockingScheduler()
 @twische.scheduled_job('interval',hours=24)
 def timed_job():
     wfutv4re.tweet()
-    wfutv4osaka,tweet()
+    wfutv4osaka.tweet()
 
 if __name__ == "__main__":
     twische.start()
